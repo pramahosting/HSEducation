02f71788dd0b34585a5ec59099f85e996288a171
pramahosting <pramahosting@gmail.com>
2026-06-07 20:32:31 +1000

    Delete frontend-service directory

--- frontend-service/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export PORT=${PORT:-3000}
-echo "[frontend] Starting on port $PORT"
-envsubst '${PORT}' < /etc/nginx/conf.d/app.conf > /tmp/app.conf
-cp /tmp/app.conf /etc/nginx/conf.d/app.conf
-exec "$@"
